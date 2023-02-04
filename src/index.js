16b04c59e85456d1b7efd4401a57a9203a1c391f
WATBO Yvan <104699044+Zamorano237@users.noreply.github.com>
2023-02-04 18:18:21 +0100

    udpate 2  no toast when the user login

--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ import 'slick-carousel/slick/slick-theme.css';
 import App from './App';
 import store from './redux';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <Router>
     <Provider store={store}>
